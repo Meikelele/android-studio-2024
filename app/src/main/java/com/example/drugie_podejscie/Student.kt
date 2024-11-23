5225873b844c3c9a64027aed0c9411fc3823bdb1
Meikelele <michal.bak2002@interia.pl>
2024-11-23 01:22:17 +0100

    added Studen model

---
+++ app/src/main/java/com/example/drugie_podejscie/Student.kt
@@ -0,0 +1,9 @@
+package com.example.drugie_podejscie
+
+data class Student (
+    val id: Int = 0,
+    val name: String,
+    val surname: String,
+    val university: String,
+    val age: Int
+    )
