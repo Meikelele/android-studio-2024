4b71d6726eb8bb2244dac94a6f920cb9ef430049
Meikelele <michal.bak2002@interia.pl>
2024-11-29 17:28:13 +0100

    added deleted button for fragments

--- app/src/main/java/com/example/drugie_podejscie/SecondActivity.kt
+++ app/src/main/java/com/example/drugie_podejscie/SecondActivity.kt
@@ -1,14 +1,17 @@
 package com.example.drugie_podejscie
 
-
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.drugie_podejscie.fragments.FirstFragment
 import com.example.drugie_podejscie.fragments.SecondFragment
-import android.widget.Button
 
 class SecondActivity : AppCompatActivity() {
+
+    private var isFirstFragmentRemoved = false
+    private var isSecondFragmentRemoved = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
@@ -18,23 +21,57 @@ class SecondActivity : AppCompatActivity() {
             .replace(R.id.fragmentContainer, FirstFragment())
             .commit()
 
-        // buttons for loading fragments
+        // Buttons for loading and removing fragments
         val f1Btn = findViewById<Button>(R.id.f1Btn)
         val f2Btn = findViewById<Button>(R.id.f2Btn)
-
+        val removeFragmentBtn = findViewById<Button>(R.id.removeFragmentBtn)
 
         f1Btn.setOnClickListener {
-            replaceFragment(FirstFragment())
+            if (!isFirstFragmentRemoved) {
+                replaceFragment(FirstFragment())
+            } else {
+                clearFrame()
+            }
         }
 
         f2Btn.setOnClickListener {
-            replaceFragment(SecondFragment())
+            if (!isSecondFragmentRemoved) {
+                replaceFragment(SecondFragment())
+            } else {
+                clearFrame()
+            }
+        }
+
+        removeFragmentBtn.setOnClickListener {
+            removeFragment()
         }
     }
 
     private fun replaceFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragmentContainer, fragment)
+            .commit()
+    }
+
+    private fun removeFragment() {
+        val fragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer)
+        if (fragment != null) {
+            supportFragmentManager.beginTransaction()
+                .remove(fragment)
+                .commit()
+
+            // Update fragment removal state
+            when (fragment) {
+                is FirstFragment -> isFirstFragmentRemoved = true
+                is SecondFragment -> isSecondFragmentRemoved = true
+            }
+        }
+    }
+
+    private fun clearFrame() {
+        // Wyświetlenie pustego widoku
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, Fragment())
             .commit()
     }
 }
